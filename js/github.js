dd1cbe38143a7f481463a801394e22f6c95ecc2b
Andrew Reynolds <andrew_reynolds@mac.com>
2020-07-20 08:50:02 -0700

    updated github constructor

--- js/github.js
+++ js/github.js
@@ -1,7 +1,7 @@
 class GitHub{
   constructor(){
-    this.client_id = '67e4256d4c8820a1b182';
-    this.client_secret = 'c0789615153eab6d7380c9d9e8151b96b9ba5b7b';
+    this.client_id = apiInfo.client_id;
+    this.client_secret = apiInfo.client_secret;
   }
 
   async getUser(user){
